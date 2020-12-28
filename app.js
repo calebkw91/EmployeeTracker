916c8d21787c71e3a08bb4d7f3612463ec0ec48a
calebkw <walke1291@gmail.com>
2020-12-28 09:52:07 -0600

    Added opening menu and required functions

--- app.js
+++ app.js
@@ -8,13 +8,97 @@ var connection = mysql.createConnection({
     user: "root",
     password: "Mintplant12!",
     database: "employees_db"
-  });
+});
 
-  connection.connect(function(err) {
+connection.connect(function(err) 
+{
     if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
+        console.error("error connecting: " + err.stack);
+        return;
     }
-  
+
     console.log("connected as id " + connection.threadId);
-  });
+});
+
+let menu = async () =>
+{
+    const menu = await inquirer.prompt(
+        [{
+            type: "list",
+            message: "What would you like to do? ",
+            name: "choice",
+            choices: [
+                'View all employees',
+                'View all employees by department',
+                'View all employees by manager',
+                'Add employee',
+                'Remove employee',
+                'Update employee role',
+                'Update employee manager',
+                'Add department',
+                'Add role',
+                'Exit'
+            ]
+        }]);
+
+    switch (menu.choice) 
+    {
+        case 'View all employees':
+            viewAll();
+            break;
+        case 'View all employees by department':
+            // viewAllByDept();
+            break;
+        case 'View all employees by manager':
+            // viewAllByManager();
+            break;
+        case 'Add employee':
+            addEmployee();
+            break;
+        case 'Remove employee':
+            // removeEmployee();
+            break;
+        case 'Update employee role':
+            updateRole();
+            break;
+        case 'Update employee manager':
+            // updateManager();
+            break;
+        case 'Add department':
+            addDepartment();
+            break;
+        case 'Add role':
+            addRole();
+            break;
+        case 'Exit':
+            console.log("Closing application");
+            break;
+        default:
+            return;
+    }
+}
+
+let viewAll = () => 
+{
+
+}
+
+let addEmployee = () =>
+{
+
+}
+
+let updateRole = () =>
+{
+
+}
+
+let addDepartment = () =>
+{
+
+}
+
+let addRole = () =>
+{
+    
+}
--- employee_db.sql
+++ employee_db.sql
@@ -25,6 +25,31 @@ CREATE TABLE employee (
   first_name varchar(30) NOT NULL,
   last_name varchar(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT NOT NULL,
+  manager_id INT,
   PRIMARY KEY (id)
 );
+
+INSERT INTO department (dpname)
+VALUES ('Sales'),
+       ('Engineering'),
+       ('Financial'),
+       ('Legal');
+       
+INSERT INTO emp_role (title, salary, department_id)
+VALUES ('Sales Lead', '100000', 1),
+	   ('Salesperson', '80000', 1),
+       ('Lead Engineer', '150000', 2),
+       ('Software Engineer', '120000', 2),
+       ('Accountant', '125000', 3),
+       ('Legal Team Lead', '250000', 4),
+       ('Lawyer', '190000', 4);
+       
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, 3),
+	   ('Mike', 'Chan', 2, 1),
+       ('Ashley', 'Rodriquez', 3, null),
+       ('Kevin', 'Tupik', 4, 3),
+       ('Malia', 'Brown', 5, null),
+       ('Sarah', 'Lourd', 6, null),
+       ('Tom', 'Allen', 7, 7),
+       ('Tammer', 'Galal', 4, 6);
